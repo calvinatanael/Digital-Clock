8a0e36d0bf6d5b78914c7e189569b327d8df192c
Calvin Natanael <calvinatanael@gmail.com>
2022-11-07 04:23:03 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+	jam();
+}
+
+function jam() {
+	let a = document.getElementById('jam'),
+	d = new Date(), h, m, s;
+	h = d.getHours();
+	m = set(d.getMinutes());
+	s = set(d.getSeconds());
+
+	a.innerHTML = h + ":" + m + ":" + s;
+
+	setTimeout('jam()', 1000);
+}
+
+function set(a) {
+	a = a < 10 ? '0' + a : a;
+	return a;
+}
+
